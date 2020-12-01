11af9c3bfb8dc3b8ee6205d1de639b4101f8a9f4
Yusuke <now.good.1206@gmail.com>
2020-12-01 12:02:21 +0900

    進捗

--- asagao/app/helpers/application_helper.rb
+++ asagao/app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
         title = "Mornig Glory"
         title = @page_title + "-" title if @page_title
     end
+
+    def menu_link_to(text,path,options = {})
+        content_tag :li do
+            link_to_unless_current(text,path,options) do
+                content_tag(:span,text)
+            end
+        end
+    end
 end
